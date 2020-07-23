5b23f926b9a1c6ba727d74203ff08a9c22bbd00c
Will Harris <will.t.harris@protonmail.com>
2020-07-23 16:40:20 -0700

    scaffold app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [registerUsername, setRegisterUsername] = useState("");
+	const [registerPassword, setRegisterPassword] = useState("");
+	const [loginUsername, setLoginUsername] = useState("");
+	const [loginPassword, setLoginPassword] = useState("");
+	const [data, setData] = useState(null);
+
+	const register = () => {};
+	const login = () => {};
+	const getUser = () => {};
+
+	return (
+		<div className="App">
+			<div>
+				<h1>Register</h1>
+				<input
+					placeholder="username"
+					onChange={(event) => setRegisterUsername(event.target.value)}
+				/>
+				<input
+					placeholder="password"
+					onChange={(event) => setRegisterPassword(event.target.value)}
+				/>
+				<button onClick={register}>Submit</button>
+			</div>
+
+			<div>
+				<h1>Login</h1>
+				<input
+					placeholder="username"
+					onChange={(event) => setLoginUsername(event.target.value)}
+				/>
+				<input
+					placeholder="password"
+					onChange={(event) => setLoginPassword(event.target.value)}
+				/>
+				<button onClick={login}>Submit</button>
+			</div>
+
+			<div>
+				<h1>Get User</h1>
+				<button onClick={getUser}>Submit</button>
+				{data ? <h1>Welcome Back {data.username}</h1> : null}
+			</div>
+		</div>
+	);
 }
 
 export default App;
