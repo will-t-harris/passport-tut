3cc202d01b2b614a939ac1b4ea79ecebd2487f93
Will Harris <will.t.harris@protonmail.com>
2020-07-25 14:28:31 -0700

    render TodoItem components in TodoList

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,5 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { TodoItem } from "../components/TodoItem";
 
 export const TodoList = () => {
-	return <div>From the list</div>;
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/")
+			.then((res) => setTodos(res.data))
+			.catch((err) => `Error in Axios request: ${err}`);
+	}, []);
+
+	const deleteTodo = (id) => {
+		axios
+			.delete(`http://localhost:5000/todos/${id}`)
+			.then((res) => console.log(res.data))
+			.catch((err) => console.error(`Error in deleteTodo: ${err}`));
+
+		const filteredTodos = todos.filter((todo) => todo._id !== id);
+		setTodos(filteredTodos);
+	};
+
+	return (
+		<div>
+			{todos.map((todo) => (
+				<TodoItem
+					key={todo._id}
+					id={todo._id}
+					todoContent={todo.todoContent}
+					todoPriority={todo.todoPriority}
+					todoDate={todo.todoDate}
+					deleteTodo={deleteTodo}
+				/>
+			))}
+		</div>
+	);
 };
