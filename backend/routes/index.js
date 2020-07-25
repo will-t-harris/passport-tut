96b72ad967bc57cf77f3f9e0534eb0dc1d0e8c71
Will Harris <will.t.harris@protonmail.com>
2020-07-25 14:27:45 -0700

    start adding routes to router

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const userController = require("../controllers/userController");
+const todoController = require("../controllers/todoController");
+
+router.get("/", todoController.getAllTodos);
+
+router.post("/add", todoController.addTodo);
+
+router.delete("/todos/:id", todoController.deleteTodoById);
+
+module.exports = router;
