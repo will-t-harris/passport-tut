d632ea64c645e2bb75c5e17b1f26c277dc9df61c
Will Harris <will.t.harris@protonmail.com>
2020-07-28 08:47:26 -0700

    add get and edit todo by id

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -10,6 +10,12 @@ exports.getAllTodos = (req, res) => {
 		.catch((err) => res.status(400).json(`Error in getAllTodos: ${err}`));
 };
 
+exports.getTodoById = async (req, res) => {
+	Todo.findById(req.params.id)
+		.then((todo) => res.json(todo))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
 exports.addTodo = (req, res) => {
 	const newTodo = new Todo({
 		todoContent: req.body.todoContent,
@@ -27,4 +33,17 @@ exports.deleteTodoById = (req, res) => {
 	Todo.findByIdAndDelete(req.params.id)
 		.then(() => res.json("Todo deleted"))
 		.catch((err) => res.status(400).json(`Error in deleteTodoById: ${err}`));
+};
+
+exports.editTodoById = (req, res) => {
+	Todo.findById(req.params.id).then((todo) => {
+		(todo.todoContent = req.body.todoContent),
+			(todo.todoPriority = req.body.todoPriority),
+			(todo.todoDate = req.body.todoDate);
+
+		todo
+			.save()
+			.then(() => res.json("Todo Updated"))
+			.catch((err) => `Error saving todo: ${err}`);
+	});
 };
